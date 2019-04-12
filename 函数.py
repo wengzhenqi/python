99994c3b5555e1d03c8db3a39637db9249113c54
翁 <46810587+wengzhenqi@users.noreply.github.com>
2019-04-12 17:10:41 +0800

    Add files via upload

--- 函数.py
+++ 函数.py
@@ -1,18 +1,20 @@
 
-def ooxx():
+def ooxx(n):
     fib = [0, 1]
-    n = int(input('请输入长度: '))
 
     for i in range( n - 2):
         fib.append(fib[-1] + fib[-2])
     # print(fib)
     return fib  #函数运算的结果需要使用return返回值,否则None
 
-mysql_list = ooxx()
+mysql_list = ooxx(10)
 print(mysql_list)
 new_list = [10 + i for i in mysql_list]
 print(new_list)
-#
+
+n = int(input('请输入长度: '))
+pp = ooxx(n)
+print(pp)
 # ooxx()
 # ooxx()
 # ooxx()
