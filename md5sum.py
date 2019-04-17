b006715d3d7d04cb298ea778ae2a3918a7486028
翁 <46810587+wengzhenqi@users.noreply.github.com>
2019-04-17 14:30:47 +0800

    Add files via upload

--- md5sum.py
+++ md5sum.py
@@ -1,12 +1,26 @@
 import sys
 import hashlib
+import os
 
-def a(fname):
-    with open(fname, 'rb') as ll:
-        kk = ll.read()
-        aa = hashlib.md5(kk)
-    p = aa.hexdigest()
-    print(p)
+
+def check_md5(fname):
+    m = hashlib.md5()
+    with open(fname, 'rb') as fobj:
+        while True:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+
+    return m.hexdigest()
 
 if __name__ == '__main__':
-    a(sys.argv[1])
+    try:
+        fname = sys.argv[1]
+    except IndexError:
+        print('Usage: %s iflename' % sys.argv[0])
+        exit(1) #$?的值为1
+    if not os.path.isfile(fname):
+        print('No such file: %s' % fname)
+        exit(2)
+    print(check_md5(fname))
