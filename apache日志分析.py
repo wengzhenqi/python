255319c0ffe61c21d5b59656be1ed90867379300
翁 <46810587+wengzhenqi@users.noreply.github.com>
2019-04-18 19:07:11 +0800

    Add files via upload

---
+++ apache日志分析.py
@@ -0,0 +1,21 @@
+import re
+
+def count_patt(fname, patt):
+    patt_dict = {}
+    cpatt = re.compile(patt)
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:   #如果匹配到内容，更新字典，None表示False
+                 key = m.group()
+                 patt_dict[key] = patt_dict.get(key, 0) + 1
+    return patt_dict
+
+
+
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = '^(\d+\.){3}\d+'
+    br = 'Chrome|Firefox|MSIE'
+    print(count_patt(fname, ip))
+    print(count_patt(fname, br))
