45553b00fe90f2002fded2e6e55d1e0b746ce210
翁 <46810587+wengzhenqi@users.noreply.github.com>
2019-04-19 10:25:00 +0800

    Add files via upload

--- 增强版访问分析.py
+++ 增强版访问分析.py
@@ -1,20 +1,20 @@
 import re
-
+from collections import Counter
 
 class countppatt:
     def __init__(self, fname):
         self.fname = fname
 
     def count_patt(self, patt):
-        patt_dict = {}
+        counter = Counter()
         cpatt = re.compile(patt)
         with open(self.fname) as fobj:
             for line in fobj:
                 m = cpatt.search(line)
                 if m:   #如果匹配到内容，更新字典，None表示False
                      key = m.group()
-                     patt_dict[key] = patt_dict.get(key, 0) + 1
-        return patt_dict
+                     counter.update([key])
+        return counter
 
 
 
@@ -22,6 +22,6 @@ if __name__ == '__main__':
     fname = 'access_log'
     ip = '^(\d+\.){3}\d+'
     cp = countppatt(fname)
-    xx = '(Chrome|Firefox|MSIE)'
-    print(cp.count_patt(ip))
-    print(cp.count_patt(xx))
+    result = cp.count_patt(ip)
+    print(result)
+    print(result.most_common(3))
