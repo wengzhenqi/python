d46e164da1dd2c7f52046430b9d07b5c986a7161
翁 <46810587+wengzhenqi@users.noreply.github.com>
2019-04-20 15:14:42 +0800

    Add files via upload

---
+++ 自定义输入文本文件.py
@@ -0,0 +1,28 @@
+import os
+
+def get_fname():
+    while True:
+        fname = input('文件名: ')
+        if not os.path.exists(fname):
+            break
+        print('已存在,请重试')
+    return fname
+
+def get_content():
+    content = []
+    print('请输入数据,在单独一行输入end结束.')
+    while True:
+        line = input('> ')
+        if line == 'end':
+            break
+        content.append(line)
+    return content
+def wfile(fname, content):
+    with open(fname, 'w') as fobj:
+        fobj.writelines(content)
+
+if __name__ == '__main__':
+    fname = get_fname()
+    content = get_content()
+    content = [line + '\n' for line in content]
+    wfile(fname, content)
